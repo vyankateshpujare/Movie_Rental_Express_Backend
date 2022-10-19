1585008b5ebe163f75c10c621f489e00c58bb4fb
vyankateshpujare <vyankateshpujare612@gmail.com>
2022-10-19 13:24:19 +0530

    added console in error middleware

--- middleware/error.js
+++ middleware/error.js
@@ -1,5 +1,6 @@
 const winston = require("winston");
 module.exports = function (err, req, res, next) {
   winston.error(err.message);
+  console.log(err);
   res.status(500).send("something wrong");
 };
